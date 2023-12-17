977a0820f2c158c137b4af6dcdb32a2410ed2a7f
“puyz” <puyzsoft@gmail.com>
2023-12-17 23:35:11 +0300

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ss
         </a>
       </header>
     </div>
